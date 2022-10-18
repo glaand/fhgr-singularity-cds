695d5f4e060c4f3bcd94fdbd345fabf0617f2f77
André Glatzl <andre@alpinesoft.ch>
2022-10-18 08:19:00 +0000

    CUDA

--- run.sh
+++ run.sh
@@ -1,1 +1,8 @@
-singularity -s exec --bind ./code:/code .container bash -c "cd /code && $1"
+#!/bin/bash
+singularity -s exec \
+        --bind ./code:/code \
+        --bind /usr/local/:/usr/local/ \
+        --bind /usr/lib/x86_64-linux-gnu/libcuda.so.1:/usr/lib/x86_64-linux-gnu/libcuda.so.1 \
+        --bind /usr/lib/x86_64-linux-gnu/libcuda.so.515.65.01:/usr/lib/x86_64-linux-gnu/libcuda.so.515.65.01 \
+        --bind /usr/lib/x86_64-linux-gnu/libcuda.so:/usr/lib/x86_64-linux-gnu/libcuda.so \
+        .container bash -c "bash _runner_ '$@'"
